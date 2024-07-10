84f4d7e2a6607e0f89df76a7881d4c2c3c63f501
Alex Beuscher <alexbeuscher@gmail.com>
2024-07-09 20:54:10 -0400

    Added stuff

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,47 +1,43 @@
 // components/Card.tsx
 
-import { Card as CardType } from '@/types';
-import React from 'react';
-import styles from './Card.module.scss';
-import { useDrag } from 'react-dnd';
+import { Card as CardType } from '@/types'
+import React from 'react'
+import styles from './Card.module.scss'
+import { useDrag } from 'react-dnd'
 
 interface CardProps {
-  card: CardType;
-  onClick: () => void;
-  onDrop: (event: React.DragEvent) => void;
+  card: CardType
+  onClick: () => void
+  onDrop: (event: React.DragEvent) => void
+  draggable?: boolean
 }
 
-const Card: React.FC<CardProps> = ({ card, onClick, onDrop }) => {
+const Card: React.FC<CardProps> = ({ card, onClick, onDrop, draggable }) => {
   const [{ isDragging }, dragRef] = useDrag(
     () => ({
       type: 'CARD',
       item: { id: card.id },
-      canDrag: card.draggable,
+      canDrag: draggable,
       collect: (monitor) => ({
-        isDragging: monitor.isDragging(),
-      }),
+        isDragging: monitor.isDragging()
+      })
     }),
-    [card.draggable]
-  );
+    [draggable]
+  )
 
   const getCardClass = (card: CardType) => {
     console.log('Card:', card)
-    const suitClass = styles[`card-${card.suit.toLowerCase()}`];
-    const rankClass = styles[`card-${card.rank.toLowerCase()}`];
-    const faceClass = card.faceUp ? '' : styles['card-facedown'];
-    return `${styles.card} ${suitClass} ${rankClass} ${faceClass}`;
-  };
-
-  return (
-    <div
-      ref={card.draggable ? dragRef : null}
-      className={getCardClass(card)}
-      onClick={onClick}
-      onDrop={onDrop}
-    >
+    const suitClass = styles[`card-${card.suit.toLowerCase()}`]
+    const rankClass = styles[`card-${card.rank.charAt(0).toLowerCase()}`]
+    const faceClass = card.faceUp ? '' : styles['card-facedown']
+    return `${styles.card} ${suitClass} ${rankClass} ${faceClass}`
+  }
+  const CardComponent = () => (
+    <div className={getCardClass(card)} onClick={onClick} onDrop={onDrop}>
       <span></span>
     </div>
-  );
-};
+  )
+  return draggable ? dragRef(<CardComponent />) : <CardComponent />
+}
 
-export default Card;
+export default Card
--- src/components/ExampleBoard/index.tsx
+++ src/components/ExampleBoard/index.tsx
@@ -1,46 +1,44 @@
-'use client';
+'use client'
 
 // Example usage in a parent component
 
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import CardStack from '@/components/CardStack';
-import { CardStack as CardStackType } from '@/types';
+import CardStack from '@/components/CardStack'
+import { CardStack as CardStackType } from '@/types'
 
 const initialStack: CardStackType = {
   id: 'stack-1',
   cards: [
-    { id: '1', suit: 'Hearts', rank: 'Ace', faceUp: true },
-    { id: '2', suit: 'Diamonds', rank: 'King', faceUp: false },
-  ],
-};
+    { id: '1', suit: 'Hearts', rank: '6', faceUp: true },
+    { id: '2', suit: 'Diamonds', rank: 'King', faceUp: false }
+  ]
+}
 const secondInitialStack: CardStackType = {
   id: 'stack-2',
-  cards: [
-    { id: '1', suit: 'Hearts', rank: 'Ace', faceUp: true },
-  ],
-};
+  cards: [{ id: '1', suit: 'Hearts', rank: '7', faceUp: true }]
+}
 
 const Example: React.FC = () => {
-  const [stack, setStack] = useState<CardStackType>(initialStack);
-  const [secondStack, setSecondStack] = useState<CardStackType>(secondInitialStack);
+  const [stack, setStack] = useState<CardStackType>(initialStack)
+  const [secondStack, setSecondStack] = useState<CardStackType>(secondInitialStack)
 
   const handleCardClick = (cardId: string) => {
     // Handle card click
-    console.log('Card clicked:', cardId);
-  };
+    console.log('Card clicked:', cardId)
+  }
 
   const handleCardDrop = (cardId: string, stackId: string) => {
     // Handle card drop
-    console.log('Card dropped:', cardId, 'into stack:', stackId);
-  };
+    console.log('Card dropped:', cardId, 'into stack:', stackId)
+  }
 
   return (
     <div className="app">
       <CardStack stack={stack} onCardClick={handleCardClick} onCardDrop={handleCardDrop} />
       <CardStack stack={secondStack} onCardClick={handleCardClick} onCardDrop={handleCardDrop} />
     </div>
-  );
-};
+  )
+}
 
-export default Example;
+export default Example
